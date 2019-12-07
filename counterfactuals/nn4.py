49da04504fac8eb37631d2525e445cf190353211
j1nma <j1nma@users.noreply.github.com>
2019-12-07 03:52:08 +0100

    styling

--- counterfactuals/nn4.py
+++ counterfactuals/nn4.py
@@ -212,7 +212,7 @@ def run(args, outdir):
                                                                                       test_score[2]))
 
     # Save means and stds NDArray values for inference
-    mx.nd.save(outdir + 'nn4_means_stds_ihdp_' + str(train_experiments) + '_.nd',
+    mx.nd.save(outdir + args.architecture.lower() + '_means_stds_ihdp_' + str(train_experiments) + '_.nd',
                {"means": mx.nd.array(means), "stds": mx.nd.array(stds)})
 
     # Export trained model
@@ -221,11 +221,11 @@ def run(args, outdir):
     print('\n{} architecture total scores:'.format(args.architecture.upper()))
 
     means, stds = np.mean(train_scores, axis=0), sem(train_scores, axis=0, ddof=0)
-    train_total_scores_str = 'train RMSE ITE: {:.2f}±{:.2f}, train ATE: {:.2f}±{:.2f}, train PEHE: {:.2f}±{:.2f}' \
+    train_total_scores_str = 'train RMSE ITE: {:.2f} ± {:.2f}, train ATE: {:.2f} ± {:.2f}, train PEHE: {:.2f} ± {:.2f}' \
                              ''.format(means[0], stds[0], means[1], stds[1], means[2], stds[2])
 
     means, stds = np.mean(test_scores, axis=0), sem(test_scores, axis=0, ddof=0)
-    test_total_scores_str = 'test RMSE ITE: {:.2f}±{:.2f}, test ATE: {:.2f}±{:.2f}, test PEHE: {:.2f}±{:.2f}' \
+    test_total_scores_str = 'test RMSE ITE: {:.2f} ± {:.2f}, test ATE: {:.2f} ± {:.2f}, test PEHE: {:.2f} ± {:.2f}' \
                             ''.format(means[0], stds[0], means[1], stds[1], means[2], stds[2])
 
     print(train_total_scores_str)
@@ -301,12 +301,12 @@ def run_test(args):
         test_scores[test_experiment, :] = test_score
 
         print(
-            '[Test Replication {}/{}]: RMSE ITE: {:0.3f}, ATE: {:0.3f}, PEHE: {:0.3f},'.format(test_experiment + 1,
+            '[Test Replication {}/{}]: RMSE ITE: {:0.3f}, ATE: {:0.3f}, PEHE: {:0.3f}'.format(test_experiment + 1,
                                                                                                test_experiments,
                                                                                                test_score[0],
                                                                                                test_score[1],
                                                                                                test_score[2]))
 
     means, stds = np.mean(test_scores, axis=0), sem(test_scores, axis=0, ddof=0)
-    print('test RMSE ITE: {:.3f}±{:.3f}, test ATE: {:.3f}±{:.3f}, test PEHE: {:.3f}±{:.3f}' \
+    print('test RMSE ITE: {:.3f} ± {:.3f}, test ATE: {:.3f} ± {:.3f}, test PEHE: {:.3f} ± {:.3f}' \
           ''.format(means[0], stds[0], means[1], stds[1], means[2], stds[2]))
