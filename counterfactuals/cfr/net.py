288584e4921bf54bb76b85058fc268b95d8c3bee
j1nma <j1nma@users.noreply.github.com>
2019-12-16 01:23:00 +0100

    WassersteinLoss

--- counterfactuals/cfr/net.py
+++ counterfactuals/cfr/net.py
@@ -3,6 +3,8 @@ import numpy as np
 from mxnet.gluon import nn, HybridBlock
 from mxnet.gluon.loss import Loss
 
+from counterfactuals.cfr.util import np_pdist2sq, np_safe_sqrt
+
 BATCH_NORM_EPSILON = 1e-3
 
 
@@ -12,6 +14,7 @@ class CFRNet(nn.HybridBlock):
         nn.HybridBlock.__init__(self, **kwargs)
 
         self.input_shape = None
+        self.batch_norm = batch_norm
 
         with self.name_scope():
             self.nonlin = nn.Activation(activation='relu')
@@ -22,7 +25,7 @@ class CFRNet(nn.HybridBlock):
                                     weight_initializer=mx.init.Normal(
                                         sigma=weight_init_scale / np.sqrt(dim_input)))
 
-            if batch_norm:
+            if self.batch_norm:
                 self.rep_fc1_bn = nn.BatchNorm(epsilon=BATCH_NORM_EPSILON, in_channels=rep_hidden_size)
 
             self.rep_fc2 = nn.Dense(rep_hidden_size,
@@ -30,7 +33,7 @@ class CFRNet(nn.HybridBlock):
                                     weight_initializer=mx.init.Normal(
                                         sigma=weight_init_scale / np.sqrt(rep_hidden_size)))
 
-            if batch_norm:
+            if self.batch_norm:
                 self.rep_fc2_bn = nn.BatchNorm(epsilon=BATCH_NORM_EPSILON, in_channels=rep_hidden_size)
 
             self.rep_fc3 = nn.Dense(rep_hidden_size,
@@ -38,7 +41,7 @@ class CFRNet(nn.HybridBlock):
                                     weight_initializer=mx.init.Normal(
                                         sigma=weight_init_scale / np.sqrt(rep_hidden_size)))
 
-            if batch_norm:
+            if self.batch_norm:
                 self.rep_fc3_bn = nn.BatchNorm(epsilon=BATCH_NORM_EPSILON, in_channels=rep_hidden_size)
 
             # Hypothesis Layers for t = 1
@@ -77,13 +80,13 @@ class CFRNet(nn.HybridBlock):
                                            sigma=weight_init_scale / np.sqrt(hyp_hidden_size)))
             self.t0_hyp_fc4 = nn.Dense(1)
 
-    def forward(self, x, t1_indices, t0_indices, batch_norm):
+    def forward(self, x, t1_indices, t0_indices):
         self.input_shape = x.shape[0]
 
-        return HybridBlock.forward(self, x, t1_indices, t0_indices, batch_norm)
+        return HybridBlock.forward(self, x, t1_indices, t0_indices)
 
-    def hybrid_forward(self, F, x, t1_indices, t0_indices, batch_norm):
-        if batch_norm:
+    def hybrid_forward(self, F, x, t1_indices, t0_indices):
+        if self.batch_norm:
             rep_relu1 = self.nonlin(self.rep_fc1_bn(self.rep_fc1(x)))
             rep_relu2 = self.nonlin(self.rep_fc2_bn(self.rep_fc2(rep_relu1)))
             rep_relu3 = self.nonlin(self.rep_fc3_bn(self.rep_fc3(rep_relu2)))
@@ -107,16 +110,67 @@ class CFRNet(nn.HybridBlock):
         return t1_hyp_relu4, t0_hyp_relu4, rep_relu3
 
 
-class Wasserstein(Loss):
-    def __init__(self, margin=6., weight=None, batch_axis=0, **kwargs):
-        super(Wasserstein, self).__init__(weight, batch_axis, **kwargs)
+class WassersteinLoss(Loss):
+    def __init__(self, p=0.5, lam=10, its=10, square=False, backpropT=False, margin=6., weight=None, batch_axis=0,
+                 **kwargs):
+        super(WassersteinLoss, self).__init__(weight, batch_axis, **kwargs)
+        self.p = p
+        self.lam = lam
+        self.its = its
+        self.square = square
+        self.backpropT = backpropT
         self.margin = margin
 
-    def hybrid_forward(self, F, image1, image2, label):
-        distances = image1 - image2
-        distances_squared = F.sum(F.square(distances), 1, keepdims=True)
-        euclidean_distances = F.sqrt(distances_squared + 0.0001)
-        d = F.clip(self.margin - euclidean_distances, 0, self.margin)
-        loss = (1 - label) * distances_squared + label * F.square(d)
-        loss = 0.5 * loss
-        return loss
+    def hybrid_forward(self, F, Xt, Xc):
+        nt = np.float(Xt.shape[0])
+        nc = np.float(Xc.shape[0])
+
+        ''' Compute distance matrix (opposite to clinicalml) '''
+        if self.square:
+            M = np_safe_sqrt(np_pdist2sq(Xt, Xc))
+        else:
+            M = np_pdist2sq(Xt, Xc)
+
+        ''' Estimate lambda and delta '''
+        M_mean = mx.nd.mean(M)
+        delta = mx.nd.stop_gradient(mx.nd.max(M))
+        eff_lam = mx.nd.stop_gradient(self.lam / M_mean)
+
+        ''' Compute new distance matrix '''
+        row = delta * mx.nd.ones_like(mx.nd.slice_axis(M, axis=0, begin=0, end=1))
+        col = mx.nd.Concat(delta * mx.nd.ones_like(mx.nd.slice_axis(M, axis=1, begin=0, end=1)),
+                           mx.nd.zeros((1, 1)), dim=0)
+        Mt = mx.nd.Concat(M, row, dim=0)
+        Mt = mx.nd.Concat(Mt, col, dim=1)
+
+        ''' Compute marginal vectors '''
+        a = mx.nd.Concat(
+            self.p * mx.nd.ones_like(mx.nd.slice_axis(Xt, axis=1, begin=0, end=1)) / nt,
+            (1 - self.p) * mx.nd.ones((1, 1)),
+            dim=0)
+
+        b = mx.nd.Concat(
+            (1 - self.p) * mx.nd.ones_like(mx.nd.slice_axis(Xc, axis=1, begin=0, end=1)) / nc,
+            self.p * mx.nd.ones((1, 1)),
+            dim=0)
+
+        ''' Compute kernel matrix'''
+        Mlam = eff_lam * Mt
+        K = mx.nd.exp(-Mlam) + 1e-6  # added constant to avoid nan
+        ainvK = K / a
+
+        u = a
+        for i in range(0, self.its):
+            u = 1.0 / (
+                mx.nd.dot(ainvK, (b / mx.nd.transpose(mx.nd.dot(mx.nd.transpose(u), K)))))
+        v = b / (mx.nd.transpose(mx.nd.dot(mx.nd.transpose(u), K)))
+
+        T = u * (mx.nd.transpose(v) * K)
+
+        if not self.backpropT:
+            T = mx.nd.stop_gradient(T)
+
+        E = T * Mt
+        D = 2 * mx.nd.sum(E)
+
+        return D
