272e3797f99a2a512b5f4633f07afd516171e6c0
j1nma <j1nma@users.noreply.github.com>
2020-01-22 04:31:25 +0100

    tsne_plot_pca fix

--- examples/mxnet/tsne_plot.py
+++ examples/mxnet/tsne_plot.py
@@ -12,7 +12,7 @@ from sklearn.manifold import TSNE
 from counterfactuals.utilities import load_data
 
 
-def tsne_plot_pca(data, label, learned_label=None, outdir='/results'):
+def tsne_plot_pca(data, label, learned_representation=None, outdir='/results'):
     X = data
     y = label
     print(X.shape, y.shape)
@@ -24,6 +24,7 @@ def tsne_plot_pca(data, label, learned_label=None, outdir='/results'):
     print('Size of the dataframe: {}'.format(df.shape))
 
     n_components = 20
+
     pca = PCA(n_components=n_components)
     pca_result = pca.fit_transform(df)
     print('Cumulative explained variation for {} principal components: {}'.format(n_components,
@@ -42,42 +43,50 @@ def tsne_plot_pca(data, label, learned_label=None, outdir='/results'):
     sns.scatterplot(
         x="tsne-pca-one",
         y="tsne-pca-two",
+        hue="y",
+        palette=sns.xkcd_palette(["windows blue", "orange"]),
         data=df,
-        legend=None,
-        alpha=0.5,
         ax=ax
     )
 
-    # START LEARNED SECTION
-    if learned_label is not None:
-        y_l = label
-        print(X.shape, y_l.shape)
+    plt.savefig(outdir + '_tsne_c={}.svg'.format(n_components), format="svg")
 
-        df_l = pd.DataFrame(X, columns=feat_cols)
+    # START LEARNED SECTION
+    if learned_representation is not None:
+        y_l = label
+        print(learned_representation.shape, y_l.shape)
+
+        feat_cols = ['feature' + str(i) for i in range(learned_representation.shape[1])]
+        df_l = pd.DataFrame(learned_representation, columns=feat_cols)
         df_l['y_l'] = y_l
         df_l['label_l'] = df_l['y_l'].apply(lambda i: str(i))
         print('Size of the dataframe: {}'.format(df_l.shape))
 
+        n_components = 100
+        pca = PCA(n_components=n_components)
         pca_result_l = pca.fit_transform(df_l)
         print('Cumulative explained variation for {} principal components: {}'.format(n_components,
                                                                                       np.sum(
                                                                                           pca.explained_variance_ratio_)))
 
         time_start = time.time()
+        tsne = TSNE(n_components=2, verbose=0, perplexity=40, n_iter=1000)
         tsne_pca_results_l = tsne.fit_transform(pca_result_l)
         print('t-SNE done! Time elapsed: {} seconds'.format(time.time() - time_start))
 
         df_l['tsne-pca-one-l'] = tsne_pca_results_l[:, 0]
         df_l['tsne-pca-two-l'] = tsne_pca_results_l[:, 1]
+        fig = plt.figure(figsize=(6, 6))
+        ax = fig.add_subplot(111)
         sns.scatterplot(
             x="tsne-pca-one-l",
             y="tsne-pca-two-l",
+            hue="y_l",
+            palette=sns.xkcd_palette(["windows blue", "orange"]),
             data=df_l,
             ax=ax
         )
-        fig.add_subplot(ax)
-
-    plt.savefig(outdir + '_tsne_c={}.svg'.format(n_components), format="svg")
+        plt.savefig(outdir + '_tsne_c_learned={}.svg'.format(n_components), format="svg")
 
 
 def main():
