6123e6f72a37bf217f3356eda957fe04e7f465f0
j1nma <j1nma@users.noreply.github.com>
2020-01-22 07:15:26 +0100

    replaced # with """

--- counterfactuals/cnn.py
+++ counterfactuals/cnn.py
@@ -31,7 +31,9 @@ def cnn_architecture(kernel_size=3, strides=2, pool_size=2):
 
 
 def run(args, outdir):
-    # Hyperparameters
+    """ Run training for CNN architecture. """
+
+    ''' Hyperparameters '''
     epochs = int(args.iterations)
     learning_rate = float(args.learning_rate)
     wd = float(args.weight_decay)
@@ -44,35 +46,35 @@ def run(args, outdir):
     f = open(logfile, 'w')
     f.close()
 
-    # Set GPUs/CPUs
+    ''' Set GPUs/CPUs '''
     num_gpus = mx.context.num_gpus()
     num_workers = int(args.num_workers)  # replace num_workers with the number of cores
     ctx = [mx.gpu(i) for i in range(num_gpus)] if num_gpus > 0 else [mx.cpu()]
     batch_size_per_unit = int(args.batch_size_per_unit)  # mini-batch size
     batch_size = batch_size_per_unit * max(num_gpus, 1)
 
-    # Set seeds
+    ''' Set seeds '''
     for c in ctx:
         mx.random.seed(int(args.seed), c)
     np.random.seed(int(args.seed))
 
-    # Convolution Neural Network Model
+    ''' Convolution Neural Network Model '''
     net = cnn_architecture()
 
-    # Load datasets
+    ''' Load datasets '''
     train_dataset = load_data('../' + args.data_dir + args.data_train)
 
-    # Instantiate net
+    ''' Instantiate net '''
     net.initialize(init=init.Xavier(), ctx=ctx)
     net.hybridize()  # hybridize for better performance
 
-    # Plot net graph
+    ''' Plot net graph '''
     # x_sym = mx.sym.var('data')
     # sym = net(x_sym)
     # mx.viz.plot_network(sym, title=args.architecture.lower() + "_plot").view(
     #     filename=outdir + args.architecture.lower() + "_plot")
 
-    # Metric, Loss and Optimizer
+    ''' Metric, Loss and Optimizer '''
     rmse_metric = mx.metric.RMSE()
     l2_loss = gluon.loss.L2Loss()
     scheduler = mx.lr_scheduler.FactorScheduler(step=learning_rate_steps, factor=learning_rate_factor,
@@ -81,23 +83,23 @@ def run(args, outdir):
     # optimizer = mx.optimizer.RMSProp(learning_rate=learning_rate, lr_scheduler=scheduler, wd=wd)
     trainer = gluon.Trainer(net.collect_params(), optimizer=optimizer)
 
-    # Initialize train score results
+    ''' Initialize train score results '''
     train_scores = np.zeros((train_experiments, 3))
 
-    # Initialize train experiment durations
+    ''' Initialize train experiment durations '''
     train_durations = np.zeros((train_experiments, 1))
 
-    # Initialize test score results
+    ''' Initialize test score results '''
     test_scores = np.zeros((train_experiments, 3))
 
-    # Train experiments means and stds
+    ''' Train experiments means and stds '''
     means = np.array([])
     stds = np.array([])
 
-    # Train
+    ''' Train '''
     for train_experiment in range(train_experiments):
 
-        # Create training dataset
+        ''' Create training dataset '''
         x = train_dataset['x'][:, :, train_experiment]
         t = np.reshape(train_dataset['t'][:, train_experiment], (-1, 1))
         yf = train_dataset['yf'][:, train_experiment]
@@ -105,7 +107,7 @@ def run(args, outdir):
         mu0 = train_dataset['mu0'][:, train_experiment]
         mu1 = train_dataset['mu1'][:, train_experiment]
 
-        # Reshaping for CNN
+        ''' Reshaping for CNN '''
         individuals_size = train_dataset['x'].shape[0]
         x = x.reshape((individuals_size, 1, 25))
         t = t.reshape((individuals_size, 1, 1))
@@ -114,7 +116,7 @@ def run(args, outdir):
 
         train, valid, test, _ = split_data_in_train_valid_test(x, t, yf, ycf, mu0, mu1)
 
-        # With-in sample
+        ''' With-in sample '''
         train_evaluator = Evaluator(np.concatenate([train['t'], valid['t']]),
                                     np.concatenate([train['yf'], valid['yf']]),
                                     y_cf=np.concatenate([train['ycf'], valid['ycf']], axis=0),
@@ -122,44 +124,44 @@ def run(args, outdir):
                                     mu1=np.concatenate([train['mu1'], valid['mu1']], axis=0))
         test_evaluator = Evaluator(test['t'], test['yf'], test['ycf'], test['mu0'], test['mu1'])
 
-        # Normalize yf
+        ''' Normalize yf '''
         yf_m, yf_std = np.mean(train['yf'], axis=0), np.std(train['yf'], axis=0)  # todo fijate bien
         train['yf'] = (train['yf'] - yf_m) / yf_std
         valid['yf'] = (valid['yf'] - yf_m) / yf_std
         test['yf'] = (test['yf'] - yf_m) / yf_std
 
-        # Save mean and std
+        ''' Save mean and std '''
         means = np.append(means, yf_m)
         stds = np.append(stds, yf_std)
 
-        # Train dataset
+        ''' Train dataset '''
         # factual_features = np.stack((train['x'], train['t']), axis=-1)
         factual_features = np.block([train['x'], train['t']])
         train_factual_dataset = gluon.data.ArrayDataset(mx.nd.array(factual_features), mx.nd.array(train['yf']))
 
-        # With-in sample
+        ''' With-in sample '''
         train_rmse_ite_dataset = gluon.data.ArrayDataset(mx.nd.array(np.concatenate([train['x'], valid['x']])))
 
-        # Valid dataset
+        ''' Valid dataset '''
         # valid_factual_features = np.stack((valid['x'], valid['t']), axis=-1)
         valid_factual_features = np.block([valid['x'], valid['t']])
         valid_factual_dataset = gluon.data.ArrayDataset(mx.nd.array(valid_factual_features), mx.nd.array(valid['yf']))
 
-        # Test dataset
+        ''' Test dataset '''
         test_rmse_ite_dataset = gluon.data.ArrayDataset(mx.nd.array(test['x']))
 
-        # Train DataLoader
+        ''' Train DataLoader '''
         train_factual_loader = gluon.data.DataLoader(train_factual_dataset, batch_size=batch_size, shuffle=True,
                                                      num_workers=num_workers)
         train_rmse_ite_loader = gluon.data.DataLoader(train_rmse_ite_dataset, batch_size=batch_size,
                                                       shuffle=False,
                                                       num_workers=num_workers)
 
-        # Valid DataLoader
+        ''' Valid DataLoader '''
         valid_factual_loader = gluon.data.DataLoader(valid_factual_dataset, batch_size=batch_size, shuffle=False,
                                                      num_workers=num_workers)
 
-        # Test DataLoader
+        ''' Test DataLoader '''
         test_rmse_ite_loader = gluon.data.DataLoader(test_rmse_ite_dataset, batch_size=batch_size,
                                                      shuffle=False,
                                                      num_workers=num_workers)
@@ -168,7 +170,7 @@ def run(args, outdir):
 
         train_start = time.time()
 
-        # Train model
+        ''' Train model '''
         for epoch in range(1, epochs + 1):  # start with epoch 1 for easier learning rate calculation
 
             start = time.time()
@@ -176,20 +178,20 @@ def run(args, outdir):
             rmse_metric.reset()
 
             for i, (batch_f_features, batch_yf) in enumerate(train_factual_loader):
-                # Get data and labels into slices and copy each slice into a context.
+                ''' Get data and labels into slices and copy each slice into a context. '''
                 batch_f_features = gluon.utils.split_and_load(batch_f_features, ctx_list=ctx, even_split=False)
                 batch_yf = gluon.utils.split_and_load(batch_yf, ctx_list=ctx, even_split=False)
 
-                # Forward
+                ''' Forward '''
                 with autograd.record():
                     outputs = [net(x) for x in batch_f_features]
                     loss = [l2_loss(yhat, y) for yhat, y in zip(outputs, batch_yf)]
 
-                # Backward
+                ''' Backward '''
                 for l in loss:
                     l.backward()
 
-                # Optimize
+                ''' Optimize '''
                 trainer.step(batch_size)
 
                 train_loss += sum([l.mean().asscalar() for l in loss]) / len(loss)
@@ -207,13 +209,13 @@ def run(args, outdir):
 
         train_durations[train_experiment, :] = time.time() - train_start
 
-        # Reshape for CNN testing
+        ''' Reshape for CNN testing '''
         train_evaluator.y_f = train_evaluator.y_f.reshape(-1)
         train_evaluator.y_cf = train_evaluator.y_cf.reshape(-1)
         test_evaluator.y_f = test_evaluator.y_f.reshape(-1)
         test_evaluator.y_cf = test_evaluator.y_cf.reshape(-1)
 
-        # Test model
+        ''' Test model '''
         y_t0, y_t1 = predict_treated_and_controlled_with_cnn(net, train_rmse_ite_loader, ctx)
         y_t0, y_t1 = y_t0 * yf_std + yf_m, y_t1 * yf_std + yf_m
         y_t0, y_t1 = y_t0.reshape(-1), y_t1.reshape(-1)
@@ -234,11 +236,11 @@ def run(args, outdir):
                                                                                       test_score[0], test_score[1],
                                                                                       test_score[2]))
 
-    # Save means and stds NDArray values for inference
+    ''' Save means and stds NDArray values for inference '''
     mx.nd.save(outdir + args.architecture.lower() + '_means_stds_ihdp_' + str(train_experiments) + '_.nd',
                {"means": mx.nd.array(means), "stds": mx.nd.array(stds)})
 
-    # Export trained model
+    ''' Export trained model '''
     net.export(outdir + args.architecture.lower() + "-ihdp-predictions-" + str(train_experiments), epoch=epochs)
 
     print('\n{} architecture total scores:'.format(args.architecture.upper()))
@@ -274,17 +276,19 @@ def run(args, outdir):
 
 
 def run_test(args):
-    # Set GPUs/CPUs
+    """ Run testing for CNN architecture. """
+
+    ''' Set GPUs/CPUs '''
     num_gpus = mx.context.num_gpus()
     num_workers = int(args.num_workers)  # replace num_workers with the number of cores
     ctx = [mx.gpu(i) for i in range(num_gpus)] if num_gpus > 0 else [mx.cpu()]
     batch_size_per_unit = int(args.batch_size_per_unit)  # mini-batch size
     batch_size = batch_size_per_unit * max(num_gpus, 1)
 
-    # Load test dataset
+    ''' Load test dataset '''
     test_dataset = load_data('../' + args.data_dir + args.data_test)
 
-    # Load training means and stds
+    ''' Load training means and stds '''
     train_means_stds = mx.nd.load(args.means_stds)
     train_means = train_means_stds['means']
     train_stds = train_means_stds['stds']
@@ -294,15 +298,15 @@ def run_test(args):
         net = gluon.nn.SymbolBlock.imports(args.symbol, ['data'], args.params,
                                            ctx=ctx)
 
-    # Calculate number of test experiments
+    ''' Calculate number of test experiments '''
     test_experiments = np.min([test_dataset['x'].shape[2], len(train_means)])
 
-    # Initialize test score results
+    ''' Initialize test score results '''
     test_scores = np.zeros((test_experiments, 3))
 
-    # Test model
+    ''' Test model '''
     for test_experiment in range(test_experiments):
-        # Create testing dataset
+        ''' Create testing dataset '''
         x = test_dataset['x'][:, :, test_experiment]
         t = np.reshape(test_dataset['t'][:, test_experiment], (-1, 1))
         yf = test_dataset['yf'][:, test_experiment]
@@ -310,21 +314,21 @@ def run_test(args):
         mu0 = test_dataset['mu0'][:, test_experiment]
         mu1 = test_dataset['mu1'][:, test_experiment]
 
-        # With-in sample
+        ''' With-in sample '''
         test_evaluator = Evaluator(t, yf, ycf, mu0, mu1)
 
-        # Retrieve training mean and std
+        ''' Retrieve training mean and std '''
         train_yf_m, train_yf_std = train_means[test_experiment].asnumpy(), train_stds[test_experiment].asnumpy()
 
-        # Test dataset
+        ''' Test dataset '''
         test_rmse_ite_dataset = gluon.data.ArrayDataset(mx.nd.array(x))
 
-        # Test DataLoader
+        ''' Test DataLoader '''
         test_rmse_ite_loader = gluon.data.DataLoader(test_rmse_ite_dataset, batch_size=batch_size,
                                                      shuffle=False,
                                                      num_workers=num_workers)
 
-        # Test model
+        ''' Test model '''
         y_t0, y_t1 = predict_treated_and_controlled(net, test_rmse_ite_loader, ctx)
         y_t0, y_t1 = y_t0 * train_yf_std + train_yf_m, y_t1 * train_yf_std + train_yf_m
         test_score = test_evaluator.get_metrics(y_t1, y_t0)
