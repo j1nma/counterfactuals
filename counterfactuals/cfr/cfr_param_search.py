1f113125571c1c38b0d2daaf9b5e2ba72223b98d
j1nma <j1nma@users.noreply.github.com>
2020-02-18 19:48:01 +0100

    cfr terminal tweaks run

--- counterfactuals/cfr/cfr_param_search.py
+++ counterfactuals/cfr/cfr_param_search.py
@@ -95,7 +95,8 @@ def run(cfg_file, num_runs):
         print('\n'.join(['%s: %s' % (str(k), str(v)) for k, v in cfg.items() if len(configs[k]) > 1]))
 
         flags = ' '.join('--%s %s' % (k, str(v)) for k, v in cfg.items())
-        call('python3 cfr/cfr_net_train.py %s' % flags, shell=True)
+        # call('python3 cfr/cfr_net_train.py %s' % flags, shell=True)  # PyCharm run
+        call('python3 counterfactuals/cfr/cfr_net_train.py %s' % flags, shell=True) # Terminal run
 
 
 if __name__ == "__main__":
